45839bc1a0186301e5f937c726917130e4091d34
Abdulaziz871 <aswadies.87@gmail.com>
2025-11-11 01:02:57 +0800

    Initial commit: Laravel + Bootstrap (Vite)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Profile</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">My Profile</a>
+        </div>
+    </nav>
+
+    <div class="container text-center mt-5">
+        <h1 class="text-primary">Welcome to My Website</h1>
+        <p class="lead">This site is built using Laravel & Bootstrap.</p>
+        <button class="btn btn-success">Get Started</button>
+    </div>
+</body>
+</html>
